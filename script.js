2b749037c3c1212f35e28617684b32422b926f50
Yanka Sikder <99686135+yawnka@users.noreply.github.com>
2024-05-16 20:23:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+  This is your site JavaScript code - you can add interactivity!
+*/
+
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+console.log("Hello 🌎");
